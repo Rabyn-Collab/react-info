94d2d1e00fa66794a69321f7c5c74511877f7506
Rabyn-Collab <babynshrestha64@gmail.com>
2023-04-23 11:21:37 +0545

    changes

--- src/Components/AddForm.js
+++ src/Components/AddForm.js
@@ -16,14 +16,16 @@ import * as Yup from 'yup';
 const AddForm = () => {
 
   const infoSchema = Yup.object().shape({
-    email: Yup.string().email().required(),
-    name: Yup.string().min(5, 'Too Short!').max(50, 'Too Long!')
-      .required('Required'),
-    gender: Yup.string().required('Required'),
-    skills: Yup.array().required('Required'),
-    country: Yup.string().required('Required'),
-    detail: Yup.string().required('Required'),
-    image: ''
+    // email: Yup.string().email().required(),
+    // name: Yup.string().min(5, 'Too Short!').max(50, 'Too Long!')
+    //   .required('Required'),
+    // gender: Yup.string().required('Required'),
+    // skills: Yup.array().required('Required'),
+    // country: Yup.string().required('Required'),
+    // detail: Yup.string().required('Required'),
+    // image: Yup.mixed().required('Required').test('File_type', 'invalid', (val) => val && ['image/png'].includes(val.type))
+
+
   });
 
   const formik = useFormik({
@@ -34,10 +36,12 @@ const AddForm = () => {
       skills: [],
       country: '',
       detail: '',
+      review: '',
       image: ''
+
     },
     onSubmit: (val) => {
-      console.log(val);
+      console.log(val.review);
     },
     validationSchema: infoSchema
   });
@@ -58,7 +62,7 @@ const AddForm = () => {
   return (
     <div className='max-w-xl p-10 shadow-xl'>
       <form onSubmit={formik.handleSubmit} className='space-y-7' >
-        <Input type='email' label='Email' name='email' onChange={formik.handleChange} value={formik.values.email} />
+        <Input error={formik.errors.email && formik.touched.email ? true : false} type='email' label='Email' name='email' onChange={formik.handleChange} value={formik.values.email} />
 
         <Input type='text' label='Your Name' name='name' onChange={formik.handleChange} value={formik.values.name} />
         {formik.errors.name && formik.touched.name && <h1 className='text-pink-500'>{formik.errors.name}</h1>}
@@ -69,6 +73,7 @@ const AddForm = () => {
             {radioData.map((d) => {
               return <Radio key={d.id} id={d.id} name={d.name} label={d.label} value={d.value} onChange={formik.handleChange} />;
             })}
+            {formik.errors.gemder && formik.touched.gender && <h1 className='text-pink-500'>{formik.errors.gender}</h1>}
 
 
           </div>
@@ -81,6 +86,7 @@ const AddForm = () => {
             {checkData.map((c) => {
               return <Checkbox key={c.id} label={c.label} name={c.name} value={c.value} onChange={formik.handleChange} />;
             })}
+            {formik.errors.skills && formik.touched.skills && <h1 className='text-pink-500'>{formik.errors.skills}</h1>}
 
 
           </div>
@@ -93,12 +99,38 @@ const AddForm = () => {
             <Option value='china'>China</Option>
             <Option value='india'>India</Option>
           </Select>
+          {formik.errors.country && formik.touched.country && <h1 className='text-pink-500'>{formik.errors.country}</h1>}
         </div>
 
         <div className="w-96">
           <Textarea label="Your Detail" />
         </div>
-        <Input type='file' label='select an image' accept='image/*' />
+        {formik.errors.name && formik.touched.name && <h1 className='text-pink-500'>{formik.errors.name}</h1>}
+
+
+
+
+        <Input
+
+          onChange={(e) => {
+            const imageFile = e.target.files[0];
+            console.log(imageFile);
+            // formik.setValues('image', imageFile);
+            const reader = new FileReader();
+            reader.readAsDataURL(imageFile);
+            reader.addEventListener('load', () => {
+              formik.setFieldValue('review', reader.result)
+            })
+
+          }}
+
+          type='file' id='image' label='select an image' name='image' accept='image/*' />
+
+
+        {/* <img src={formik.values.review} alt="" />
+        {formik.errors.image && formik.touched.image && <h1 className='text-pink-500'>{formik.errors.image}</h1>} */}
+
+
         <Button type='submit'>Submit</Button>
 
       </form>
